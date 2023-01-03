115c9366e6b7964d43df12b82e9565836e202940
Annie Bryan <annieb22@mit.edu>
2023-01-03 15:52:55 -0500

    added more cuisines and categories, changed wheel to spin for category first then cuisine, modified layout

--- cuisineFusionWheel.js
+++ cuisineFusionWheel.js
@@ -1,138 +1,8 @@
-const get = (id) => {
-  return document.getElementById(id);
-};
+import { allCuisines, cuisineMap, wildCards } from './allCuisines.js';
 
-const selectRandom = (cuisines) => {
-  const ix = Math.floor(Math.random() * cuisines.length);
-  return cuisines[ix];
-};
+const get = (id) => document.getElementById(id);
 
-const areCompatible = (cuisineA, cuisineB) => {
-  if (cuisineA === cuisineB) {
-    return false;
-  }
-  const categories = [eastAsianCuisines, southeastAsianCuisines, scandinavianCuisines, hispanicCuisines]
-  return !categories.some(category => category.includes(cuisineA) && category.includes(cuisineB));
-};
-
-const allCuisines = [
-  'American', 
-  'Brazilian', 
-  'Chilean', 
-  'Chinese', 
-  'Colombian', 
-  'Cuban', 
-  'Dominican', 
-  'Dutch', 
-  'Filipino', 
-  'French', 
-  'German', 
-  'Greek', 
-  'Indian', 
-  'Indonesian', 
-  'Italian', 
-  'Jamaican', 
-  'Japanese', 
-  'Korean', 
-  'Lebanese', 
-  'Malaysian', 
-  'Mexican', 
-  'Mongolian',
-  'Moroccan', 
-  'Nigerian', 
-  'Persian', 
-  'Peruvian', 
-  'Polish', 
-  'Spanish',
-  'Swedish',
-  'Taiwanese', 
-  'Thai', 
-  'Turkish', 
-  'Vietnamese'
-];
-
-const eastAsianCuisines = [
-  'Chinese',
-  'Japanese',
-  'Korean',
-  'Mongolian',
-  'Taiwanese'
-];
-
-const southeastAsianCuisines = [
-  'Indonesian',
-  'Malaysian',
-  'Filipino',
-  'Thai',
-  'Vietnamese'
-];
-
-const scandinavianCuisines = [
-  'Danish',
-  'Swedish'
-];
-
-const hispanicCuisines = [
-  'Chilean',
-  'Colombian',
-  'Cuban',
-  'Dominican',
-  'Mexican',
-  'Peruvian',
-  'Spanish'
-];
-
-const box = get('box');
-const cuisine1 = get('cuisine1');
-const cuisine2 = get('cuisine2');
-const resetMessage = get('reset-message');
-const button = get('button');
-
-const createWheel = (cuisines) => {
-  cuisines.forEach((cuisine, i) => {
-    const b = document.createElement('b');
-    const span = document.createElement('span');
-    b.textContent = cuisine;
-    span.appendChild(b);
-    span.classList.add(`span${i}`);
-    span.classList.add(`color${i % 3}`)
-    span.classList.add((cuisines.length >= 30) ? 'all-elements' : 'fewer-elements');
-    span.style.transform = `rotate(${i * 360 / cuisines.length}deg)`;
-    box.appendChild(span);
-  });
-};
-
-function spinWheel(cuisines) {
-  const min = 1080;
-  const max = 1800;
-  const deg = Math.floor(Math.random() * (max - min)) + min;
-
-  box.style.transition = 'all ease 5s';
-  box.style.transform = `rotate(${deg}deg)`;
-
-  const numCuisines = cuisines.length;
-  const ix = numCuisines - Math.round((deg % 360) * (numCuisines / 360));
-  const cuisine = (ix === cuisines.length) ? cuisines[0] : cuisines[ix];
-
-  return cuisine;
-}
-
-function spin(n, cuisines) {
-  resetMessage.textContent = ""
-  const cuisine = spinWheel(cuisines);
-  setTimeout(() => {
-    if (n === 1) {
-      cuisine1.textContent = `Cuisine 1: ${cuisine}`;
-      button.textContent = `RESET`;
-      button.onclick = () => reset(cuisine);
-    } else {
-      cuisine2.textContent = `Cuisine 2: ${cuisine}`;
-      b
-    }
-  }, 5000);
-}
-
-function reset(cuisine) {
+const createWheel = (options, nextOnClick) => {
   while (box.childNodes.length > 0) {
     box.removeChild(box.childNodes[0]);
   }
@@ -140,18 +10,84 @@ function reset(cuisine) {
   box.style.transition = 'all ease 0s';
   box.style.transform = 'rotate(0deg)';
 
-  const compatibleCuisines = allCuisines.filter((c) => areCompatible(cuisine, c));
-  const removedCuisines = allCuisines.filter((c) => !compatibleCuisines.includes(c));
-  createWheel(compatibleCuisines);
-  if (removedCuisines.length === 1) {
-    resetMessage.textContent = `Removed ${removedCuisines}`;
-  } else {
-    resetMessage.textContent = `Removed ${removedCuisines.length} cuisines: ${removedCuisines}`
-  }
+  console.log(options);
 
-  button.textContent = `SPIN`;
-  button.onclick = () => spin(2, compatibleCuisines);
+  const numOptions = options.length;
+  options.forEach((cuisine, i) => {
+    const text = document.createElement('p');
+    text.textContent = cuisine;
+    const span = document.createElement('span');
+    span.appendChild(text);
+    span.classList.add(`span${i}`);
+    if (numOptions % 3 === 1) {
+      span.classList.add(`color${i % 5}`);
+    } else {
+      span.classList.add(`color${i % 3}`);
+    }
+    span.classList.add(`elements-${numOptions}`);
+    span.style.transform = `rotate(${i * 360 / numOptions}deg)`;
+    box.appendChild(span);
+  });
+
+  button.textContent = 'SPIN';
+  button.onclick = nextOnClick;
+};
+
+function resetGame() {
+  result1.textContent = 'Cuisine 1: ';
+  result2.textContent = 'Cuisine 2: ';
+  wildcard.textContent = 'Wild Card: ';
+  createWheel(allCategories, () => spinWheel(allCategories, 1));
 }
 
-button.onclick = () => spin(1, allCuisines);
-createWheel(allCuisines);
+function spinWheel(options, spinNum) {
+  const numOptions = options.length;
+  const min = numOptions * 3;
+  const max = numOptions * 5;
+  const rand = Math.floor(Math.random() * (max - min)) + min;
+  const ix = (numOptions - (rand % numOptions)) % numOptions
+  const deg = rand * (360 / numOptions);
+  const selectedOption = options[ix];
+
+  box.style.transition = 'all ease 5s';
+  box.style.transform = `rotate(${deg}deg)`;
+
+  const categoryCuisines = allCuisines[selectedOption];
+
+  setTimeout(() => {
+    button.textContent = 'RESET';
+    switch(spinNum) {
+      case 1:
+        button.onclick = () => createWheel(categoryCuisines, () => spinWheel(categoryCuisines, 2));
+        break;
+      case 2:
+        result1.textContent = `Cuisine 1: ${selectedOption}`;
+        const remainingCategories = allCategories.filter(c => c !== cuisineMap[selectedOption]);
+        button.onclick = () => createWheel(remainingCategories, () => spinWheel(remainingCategories, 3));
+        break;
+      case 3:
+        button.onclick = () => createWheel(categoryCuisines, () => spinWheel(categoryCuisines, 4));
+        break;
+      case 4:
+        result2.textContent = `Cuisine 2: ${selectedOption}`;
+        button.onclick = () => createWheel(wildCards, () => spinWheel(wildCards, 5));
+        break;
+      case 5:
+        wildcard.textContent = `Wild Card: ${selectedOption}`;
+        button.onclick = resetGame;
+        button.textContent = "AGAIN";
+        break;
+    }
+  }, 5000);
+
+  return selectedOption;
+}
+
+const button = get('button');
+const box = get('box');
+const result1 = get('result1');
+const result2 = get('result2');
+const wildcard = get('wildcard');
+const allCategories = Object.keys(allCuisines);
+
+resetGame();
